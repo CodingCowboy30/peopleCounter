6b8d6ab23f8ca64124bf561f0bfdd7cdc1001ca7
Martez Carr <113547475+CodingCowboy30@users.noreply.github.com>
2023-09-27 21:26:22 -0500

    Final People counter w/Comments for examples

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-// document.getElementById("count").innerText = 5
+//document.getElementById("increment-btn").addEventListener("click", increment)= 5
+let countEl = document.getElementById("count-el") //.innerHTML ++
+let saveEl = document.getElementById("save-el")
+let count = 0
 
 function increment(){
-    console.log("This Buutton was Clicked")
+ count += 1;
+ countEl.textContent = count
 }
+
+function save(){
+    let countStr = count + " - "
+     saveEl.textContent += countStr
+     countEl.textContent = 0
+     count = 0
+}
+
+/*let username = "per"
+let message = "You have tree new notification"
+
+let messageToUser = message + ", " + username + "!"
+
+console.log(messageToUser)*/
+
+/*let username = "Slim Shady"
+let greeting = "Hi, my name is "
+let myGreeting = greeting + ", " + "Chicky, chicky, "+ username + "!"
+console.log(myGreeting)*/
+
+/*let username = "Martez Carr"
+let greeting = "Welcome Back "
+let welcomeEl = document.getElementById("welcome-el")
+
+welcomeEl.innerHTML = greeting + username*/
+
+
+
